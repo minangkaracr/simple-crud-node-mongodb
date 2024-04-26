e5e88d9d8752e40828f43f636764d44333d3c073
unknown <rengga.minangkara@gmail.com>
2024-04-26 08:03:36 +0700

    update controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,7 +2,7 @@ const Product = require('../models/product.model.js')
 
 const getProducts = async (req, res) => {
     try {
-        const products = await Product.find({})
+        const products = await Product.find({}).select('_id name quantity price')
         res.status(200).json(products)
     } catch (error) {
         res.status(500).json({message: error.message})
